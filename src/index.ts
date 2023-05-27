fa673f73c618b54739c556889d5328d5fc08e084
fergut <gutierrezfdo@gmail.com>
2023-05-27 15:39:13 -0500

    Read app port from environment variable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 
-const port = '6000';
+const port = process.env.APP_PORT ?? "6000";
 
 app.listen(port, () => {
     console.log("traffic-control-service listening in port %s", port);
